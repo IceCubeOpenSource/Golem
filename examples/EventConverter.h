a9e6a52d186851dda8e6a939c10e35b2705db4c7
arguelles <caad@mit.edu>
2018-03-06 13:50:20 -0600

    added comments to event converter class

--- examples/EventConverter.h
+++ examples/EventConverter.h
@@ -6,10 +6,14 @@
 #include "DataLoader.h"
 #include "PhysicsModel.h"
 
+/// \brief Event Converter class.
 class EventConverter {
   private:
     std::unique_ptr<LW::Weighter> weighter;
   protected:
+    /// \brief Gets a data loader event and returns its weight.
+    /// @param e_input Event from the data loader.
+    /// \details Transfer properties from the DataLoader::Event to a LeptonWeighter::Event and apply the weighter.
     double GetEventWeight(DataLoader::Event& e_input){
       LW::Event lw_e {
         e.primaryType,
@@ -29,15 +33,28 @@ class EventConverter {
       return weighter(lw_e);
     }
   public:
+    /// \brief Event converter constructor.
+    /// @param path_to_lic_file Path to Lepton Injector Configuration (LIC) file.
+    /// @param path_to_cross_sections Path to the folder where cross section splines are located.
+    /// \details Generated the generator and cross section weighters, which then are assambled
+    /// into the LeptonWeighter::Weighter.
     EventConverter(std::string path_to_lic_file, std::string path_to_cross_sections){
       std::vector<std::shared_ptr<LW::Generator>> generators = LW::MakeGeneratorsFromLICFile(path_to_lic_file);
       std::shared_ptr<LW::CrossSectionFromSpline> xs = std::make_shared<LW::CrossSectionFromSpline>(path_to_cross_sections + "/dsdxdy_nu_CC_iso.fits",
                                                                                                     path_to_cross_sections + "/dsdxdy_nubar_CC_iso.fits",
                                                                                                     path_to_cross_sections + "/dsdxdy_nu_NC_iso.fits",
                                                                                                     path_to_cross_sections + "/dsdxdy_nubar_NC_iso.fits");
+      // Constructs and sets the weighter
       weighter.reset(new LW::Weighter(xs,generators));
     }
 
+    /// \brief Converts one data loader event structure to a physics model event structure.
+    /// @param e_input Event from the data loader.
+    /// \details This function takes one event from the data loader and extracts
+    /// the properties needed for the analysis. To do so it dumps the content into
+    /// PhysicsModel::Event structure. In this particular example the analizer is only
+    /// needs energy and zenith information. Additionally the weight is calculated by
+    /// LeptonWeighter and store in the object.
     PhysicsModel::Event ConvertEvent(DataLoader::Event& e_input){
       PhysicsModel::Event e_output;
 
